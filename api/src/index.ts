506c00cb6dd3f1923e8c1e31b888282d568db625
Masanobu Ishiyama <ishiyama@MasanobunoMacBook-Pro.local>
2024-07-15 23:13:56 +0900

    wip

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const app = express();
 
-app.use("/hello", async (req, res) => {
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+
+app.use("/users", async (req, res) => {
   const prisma = new PrismaClient();
-  const user = await prisma.user.findFirst();
-  res.send("helloworld: " + user?.name)
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 const port = process.env.PORT || 3000;
